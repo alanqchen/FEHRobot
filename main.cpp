7c29089194febf858782e28e7573fc6dbd4fc3d3
Alan Chen <alanchen1908@gmail.com>
2020-02-18 02:57:42 -0500

    Started the PID function
    TODO: Implement trajectory profile parser
    profile -> kinematic relationships -> ref velocity -> integrate -> ref position

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #define MOTOR_ANGLE_2 150*M_PI/180.0
 #define MOTOR_ANGLE_3 270*M_PI/180.0
 #define MAX_RPM 236.5398195
+#define ENCODER_RES 318.0
+#define DELTA_T 0.1
 #define R 0.11176
 #define r 0.03
 
@@ -80,6 +82,78 @@ void spinClockwise(float vel, float theta) {
 // 6.6 MAX, 1.65-> 25%
 void spin(float phi, float theta) {
     kinematics(0.0, 0.0, theta, phi);
+}
+
+float inchestoMeters(float num) {
+    return num/39.37;
+}
+
+float countsToRadDisp(int new, int old) {
+    int difference = new-old;
+    //float degDif = (float)difference * 360.0/318.0;
+    //return degDif * (M_PI / 180.0);
+    return (float)difference*(2.0*M_PI)/ENCODER_RES;
+}
+
+// Give in inches (for RPS)
+void PIDMoveTo(float x, float y) {
+    float xMeters = inchestoMeters(x);
+    float yMeters = inchestoMeters(y);
+    int countNew1 = 0;
+    int countNew2 = 0;
+    int countNew3 = 0;
+    int countOld1 = 0;
+    int countOld2 = 0;
+    int countOld3 = 0;
+    float displacement1 = 0.0;
+    float displacement2 = 0.0;
+    float displacement3 = 0.0;
+    float phiVel1 = 0.0;
+    float phiVel2 = 0.0;
+    float phiVel3 = 0.0;
+    float phi1 = 0.0;
+    float phi2 = 0.0;
+    float phi3 = 0.0;
+    float motorSpeed1 = 0.0; 
+    float motorSpeed2 = 0.0; 
+    float motorSpeed3 = 0.0; 
+    float errorCurr1 = 0.0;
+    float errorCurr2 = 0.0;
+    float errorCurr3 = 0.0;
+    float errorTotal1 = 0.0;
+    float errorTotal2 = 0.0;
+    float errorTotal3 = 0.0;
+    float Kp = 1.0;
+    float Ki = 0.0;
+    float Kd = 0.0;
+    float pidMarginError = 0.1; // in inches
+    
+    bool setup = true;
+    
+    while (sqrt(pow(RPS.X(),2)+pow(RPS.Y(),2)) > pidMarginError) {
+        countNew1 = motor1_encoder.Counts();
+        countNew2 = motor2_encoder.Counts();
+        countNew3 = motor3_encoder.Counts();
+        if(setup) {
+            displacement1 = countsToRadDisp(countNew1, 0);
+            displacement2 = countsToRadDisp(countNew2, 0);
+            displacement3 = countsToRadDisp(countNew3, 0);
+            setup = false;
+        } else {
+            displacement1 = countsToRadDisp(countNew1, countOld1);
+            displacement2 = countsToRadDisp(countNew2, countOld1);
+            displacement3 = countsToRadDisp(countNew3, countOld1);
+        }
+        phi1 += displacement1;
+        phi2 += displacement2;
+        phi3 += displacement3;
+        phiVel1 = displacement1/DELTA_T;
+        phiVel2 = displacement2/DELTA_T;
+        phiVel3 = displacement3/DELTA_T;
+        
+        // TODO: IMPLEMENT TRAJECTORY PROFILE PARSER
+        // PROFILE -> PARSE USING KINEMATIC RELATIONSHIPS -> REFERENCE MOTION -> INTEGRATION -> REFERENCE POSITION
+    }
 }
 
 int main(void)
