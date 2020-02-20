1311a9541a4af0b0ced8abbf6cb9f931197f418f
Alan Chen <alanchen1908@gmail.com>
2020-02-20 01:50:49 -0500

    Added comments to the PI Controller function

--- main.cpp
+++ main.cpp
@@ -135,10 +135,27 @@ float countsToRadDisp(int newCount, int old) {
     return (float)difference*(2.0*M_PI)/ENCODER_RES;
 }
 
-// Give in inches (for RPS)
+// TODO: Change name to PIMoveTO
+/* PIDMoveTO
+ * Uses a PI controller to move the robot using a trajectory profile as the reference data.
+ *  The passed in trajectory profile should have 6 columns. The first 3 are the total angular
+ *  displacements(rad) of motors 1, 2, and 3 repsectively, with the last 3 being the respective angular
+ *  velocities(rad/s). Right now, delta time is 0.1 seconds. The loop each iteration will calculate difference in
+ *  encoder counts to convert it into displacement in radians using countsToRadDisp. This will then be
+ *  added to the total angular displacement for each motor, and these calculated values will be subracted
+ *  with the reference angular displacements to get the error for each motor. These errors are then summed
+ *  to the motors individual total error counter, and based on the error values and P & I constants,
+ *  angular velocities(rad/s) to set the motors to is calculated. Then the funtion setRadSToPercent is called to
+ *  convert the angular speeds to percent and limit them within operating range, and then set the motors to that percent.
+ *
+ * Parameters:
+ *  fName - file name of trajectory profile  
+ *  size - number of lines/commands in trajectory profile
+ */
 void PIDMoveTo(char* fName, int size) {
     //float xMeters = inchestoMeters(x);
     //float yMeters = inchestoMeters(y);
+    /* Set important variables */
     int countNew1 = 0;
     int countNew2 = 0;
     int countNew3 = 0;
@@ -170,9 +187,10 @@ void PIDMoveTo(char* fName, int size) {
     float Ki = 0.0;
     float Kd = 0.0;
     float pidMarginError = 0.1; // in inches
-    
+    // might remove this
     bool setup = true;
     
+    /* Reset encoder counts */
     motor1_encoder.ResetCounts();
     motor2_encoder.ResetCounts();
     motor3_encoder.ResetCounts();
@@ -181,19 +199,25 @@ void PIDMoveTo(char* fName, int size) {
     //int len = fName.length();
     //char charArr[len+1];
     //strcpy(charArr, fName.c_str());
+    /* Get trajectory profile from file */
     FEHFile *fptr = SD.FOpen("testRot3.txt","r");
-    // Open write file to track error over time
+    /* Open write files to track error and delta angular displacement */
+    // This is useful for tuning among other things
     FEHFile *fOutErrptr = SD.FOpen("errorLog.txt","w");
     FEHFile *fOutDispptr = SD.FOpen("dispLog.txt","w");
+    
+    /* Init 2d arrays to store reference data and other temp variables to read from file */
     float pos_ref[3][size];
     float vel_ref[3][size];
     float temp1;
     float temp2;
     float temp3;
+    /* If file failed to open, or invalid profile, return and make the screen red */
     if(SD.FEof(fptr)) {
         LCD.Clear(FEHLCD::Red);
         return;
     }
+    /* Parse trajectory file */
     int i = 0;
     while(!SD.FEof(fptr)) {
         SD.FScanf(fptr, "%f%f%f%f%f%f", &temp1, &temp2, &temp3, &refSpeed1, &refSpeed2, &refSpeed3);
@@ -205,16 +229,23 @@ void PIDMoveTo(char* fName, int size) {
         vel_ref[2][i] = refSpeed3;
         i++;
     }
+    // Should also remove this
     LCD.Clear(FEHLCD::Blue);
+    // Should remove this later
     if(i < 50) {
         LCD.Clear(FEHLCD::Black);
     }
+    /* Close trajectory file */
     SD.FClose(fptr);
-
+    /* PI LOOP */
+    // Yes, not PID as the derivative term isn't needed currently
     for (int i = 0; i < size; i++) {
+        /* Get new encoder counts */
         countNew1 = motor1_encoder.Counts();
         countNew2 = motor2_encoder.Counts();
         countNew3 = motor3_encoder.Counts();
+        /* Check if first iteration (no old values) */
+        // This is redundant! (remove later)
         if(setup) {
             displacement1 = countsToRadDisp(countNew1, 0);
             displacement2 = countsToRadDisp(countNew2, 0);
@@ -225,36 +256,48 @@ void PIDMoveTo(char* fName, int size) {
             displacement2 = countsToRadDisp(countNew2, countOld2);
             displacement3 = countsToRadDisp(countNew3, countOld3);
         }
+        // Set old counts to new counts for the next iteration
         countOld1 = countNew1;
         countOld2 = countNew2;
         countOld3 = countNew3;
+        // Add to total angular displacement
         phi1 += displacement1;
         phi2 += displacement2;
         phi3 += displacement3;
+        // Calc average angular velocity (this will be used if we switch to a angular velocity trajectory profile)
         phiVel1 = displacement1/DELTA_T;
         phiVel2 = displacement2/DELTA_T;
         phiVel3 = displacement3/DELTA_T;
         
+        // Write to log file
         SD.FPrintf(fOutDispptr, "%f\t%f\t%f\n", displacement1, displacement2, displacement3);
 
-        // TODO: IMPLEMENT TRAJECTORY PROFILE PARSER
-        // PROFILE -> PARSE USING KINEMATIC RELATIONSHIPS -> REFERENCE MOTION -> INTEGRATION -> REFERENCE POSITION
+        // TODO: IMPLEMENT DEAD BAND
+        
+        /* Calculate current error relative to reference angular positions for each encoder */
         errorCurr1 = pos_ref[0][i] - phi1;
         errorCurr2 = pos_ref[1][i] - phi2;
         errorCurr3 = pos_ref[2][i] - phi3;
-
+        
+        // Saftey check in case something goes terribly wrong, may or may not be needed later
         if(errorCurr1 > 3)
             return;
-
+        
+        // Write errors to log file
         SD.FPrintf(fOutErrptr, "%f\t%f\t%f\n", errorCurr1, errorCurr2, errorCurr3);
+        // Add to total error (for integral term)
         errorTotal1 += errorCurr1;
         errorTotal2 += errorCurr2;
         errorTotal3 += errorCurr3;
-
+        
+        // TODO: ADD INTERGRAL WINDUP REMOVAL
+        
+        /* Calc motor speeds (rad/s) using P and I */
         motorSpeed1 = Kp * errorCurr1 + Ki * DELTA_T * (errorTotal1);
         motorSpeed2 = Kp * errorCurr2 + Ki * DELTA_T * (errorTotal2);
         motorSpeed3 = Kp * errorCurr3 + Ki * DELTA_T * (errorTotal3);
-
+        
+        /* Use the reference velocities to determine if motor speed should change signs */
         if(vel_ref[0][i] < 0.0) {
             motorSpeed1 *= -1.0;
         }
@@ -268,10 +311,14 @@ void PIDMoveTo(char* fName, int size) {
         //motor1.SetPercent(limitMotorPercent(motorSpeed1));
         //motor2.SetPercent(limitMotorPercent(motorSpeed2));
         //motor3.SetPercent(limitMotorPercent(motorSpeed3));
+        /* Set motors to speed */
         setRadSToPercent(motorSpeed1, motorSpeed2, motorSpeed3);
+        /* Wait 0.1 seconds (100 miliseconds) */
         Sleep(100);
     }
+    /* Done with trajectory profile, stop all motors */
     allStop();
+    /* Close all log files */
     SD.FClose(fOutErrptr);
     SD.FClose(fOutDispptr);
 }
